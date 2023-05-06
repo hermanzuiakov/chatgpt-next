7601edb666d9a17f1e205482f4a1126bbf82a2b6
Herman Zuiakov <gzuyakov@gmail.com>
2023-05-06 18:38:42 +0200

    Creating UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ToastProvider } from '@apideck/components'
+import { AppProps } from 'next/app'
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+      <ToastProvider>
+        <Component {...pageProps} />
+      </ToastProvider>
+  )
 }
